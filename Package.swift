43118cd0b7f65afbe8d0da19ea7a7a249495b623
Joannis Orlandos <joannis@orlandos.nl>
2016-04-06 11:55:02 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,8 @@ import PackageDescription
 let package = Package(
     name: "SCRAM",
     dependencies: [
-        .Package(url: "https://github.com/CryptoKitten/PBKDF2.git", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/CryptoKitten/PBKDF2.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/CryptoKitten/SHA1.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/CryptoKitten/HMAC.git", majorVersion: 0, minor: 2)
     ]
 )
